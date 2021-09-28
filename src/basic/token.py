4b59a656b6dd0ba53faf4d9d1bc5444634b44990
haldihri3 <Houida>
2021-09-27 21:15:19 -0400

    Removed unnecessary changes

--- src/basic/token.py
+++
@@ -1,8 +0,0 @@
-# class Token:
-#   def __init__(self, type_, value=None):
-#         self.type = type_
-#         self.value = value
-        
-#   def __repr__(self):
-#         if self.value: return f'{self.type}:{self.value}'
-#         return f'{self.type}'
